96f1363d249e9e4592993d39d5e84f6ddd730108
AlexAraldi <alexanderaraldi@gmail.com>
2025-10-14 17:44:43 -0300

    implementado configurações de rota e ajustes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,11 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { MidiaService } from './services/midia-service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 import { Navbar } from './components/navbar/navbar';
-import { BannerPrincipal } from './components/banner-principal/banner-principal';
 
 @Component({
   selector: 'app-root',
-  imports: [AsyncPipe, Navbar, BannerPrincipal],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly midiaService = inject(MidiaService);
-
-  protected readonly midiasPopulares$ = this.midiaService.selecionarMidiasPopulares();
-}
+export class App {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,5 @@
-import { provideHttpClient } from '@angular/common/http';
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZonelessChangeDetection,
-} from '@angular/core';
-import { provideRouter, Routes } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-export const routes: Routes = [];
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient(),
-  ],
-};
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
